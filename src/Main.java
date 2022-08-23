4dd377eff71674d170a5d79b07808b84a5e2b9e6
ze1enyy <zelenyied@gmail.com>
2022-08-23 20:30:42 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        String _student = ("Eduard Zelenyi, KI-36");
-        System.out.println(_student);
     }
 }
---
+++ src/lab1.java
@@ -0,0 +1,8 @@
+class lab1 {
+    public static void main(String[] args)
+    {
+        String _student = "Eduard Zelenyi, KI-36";
+        System.out.println(_student);
+    }
+
+}
